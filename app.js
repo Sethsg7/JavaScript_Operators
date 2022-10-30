d48d4d7ded2a1f8265c3c94e613408a05b3779bb
Sethsg7 <Sethsg7@gmail.com>
2022-10-30 03:26:35 -0500

    OperatorsAssnmt

--- app.js
+++ app.js
@@ -7,13 +7,48 @@ console.log(
 console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiple = a * b;
+let dividing = a / b;
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
-
+```
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+```;
 // YOUR CODE HERE
+("1111");
+("11eleven");
+12;
+("Frodo11");
+("true11");
+("FrodoBaggins");
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
+```
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+```;
 // YOUR CODE HERE
+true;
+false;
+true;
+false;
+true;
+true;
+false;
+false;
+true;
+
